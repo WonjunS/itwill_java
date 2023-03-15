c0a3cdbb8d30b56024bded53aeb339e47ec33cc4
Wonjun Seo <93713151+WonjunS@users.noreply.github.com>
2023-03-15 13:41:05 +0900

    Add files via upload

---
+++ Java01_Intro/src/edu/java/intro/IntroMain.java
@@ -0,0 +1,14 @@
+package edu.java.intro;
+
+public class IntroMain {
+
+	/*
+	 * 블록 주석 (block comment)
+	 */
+	// 한줄 주석 (in-line comment): 프로그램에 대한 설명. 컴파일 시 무시됨.
+	// Main method: 프로그램의 시작
+	public static void main(String[] args) {
+		System.out.println("안녕하세요, Java!");
+	}
+
+} 
